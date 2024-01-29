04c30da9773a39ed9c0bd50a630ff6ac95f483d4
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-01-30 00:43:47 +0100

    added google maps package and key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,13 @@ export default defineNuxtConfig({
   },
   tailwindcss: {
     configPath: '~/tailwind.config.ts',
-  }
+  },
+  build: {
+    transpile: ['@fawmi/vue-google-maps']
+  },
+  runtimeConfig: {
+    public: {
+      GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
+    },
+  },
 })
