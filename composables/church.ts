676ff9fd33f5f641e4a5cca9894c3372e83e9983
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-01-30 00:45:32 +0100

    added maps localizations

---
+++ composables/church.ts
@@ -0,0 +1,11 @@
+export const useChurch = () => {
+  return {
+    'id': 1,
+    'nameId': 'church',
+    'description': 'Parafia Rzymskokatolicka pw. Podwyższenia Krzyża Świętego w Bargłowie Kościelnym',
+    'position': {
+      'lat': 53.771119153533434,
+      'lng': 22.81841075848204,
+    },
+  }
+}
---
+++ composables/wedding.ts
@@ -0,0 +1,11 @@
+export const useWedding = () => {
+  return {
+    'id': 2,
+    'nameId': 'wedding',
+    'description': 'Sala Bankietowa Uroczysko',
+    'position': {
+      'lat': 53.796224899348005,
+      'lng': 22.80819614693889,
+    }
+  }
+}
