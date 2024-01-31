d69506f3e384a92b9a7349db51b87c74e7710c09
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-02-01 00:05:18 +0100

    feat: changed id, added type and icon

--- composables/church.ts
+++ composables/church.ts
@@ -1,11 +1,14 @@
 export const useChurch = () => {
   return {
-    'id': 1,
+    'id': 0,
     'nameId': 'church',
     'description': 'Parafia Rzymskokatolicka pw. Podwyższenia Krzyża Świętego w Bargłowie Kościelnym',
+    'address': 'ul. Augustowska 23, 16-320 Bargłów Kościelny',
     'position': {
       'lat': 53.771119153533434,
       'lng': 22.81841075848204,
     },
+    'type': 'Kościół',
+    'icon': 'WeddingRings'
   }
 }
--- composables/wedding.ts
+++ composables/wedding.ts
@@ -1,11 +1,14 @@
 export const useWedding = () => {
   return {
-    'id': 2,
+    'id': 1,
     'nameId': 'wedding',
     'description': 'Sala Bankietowa Uroczysko',
+    'address': 'Nowiny Bargłowskie 3, 16-320 Bargłów Kościelny',
     'position': {
       'lat': 53.796224899348005,
       'lng': 22.80819614693889,
-    }
+    },
+    'type': 'Sala weselna',
+    'icon': 'Dance',
   }
 }
