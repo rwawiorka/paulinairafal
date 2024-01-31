a6db8dec5a58e4be15f73a9bff1e81ae01fa28e7
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-02-01 00:06:02 +0100

    feat: added scroll to maps on click

--- components/Button.vue
+++ components/Button.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <UButton
-      class="wedding-300 absolute top-[calc(83%)] left-1/2 -translate-x-1/2 text-slate-200 p-5 font-alegreya font-bold text-xl wedding-box-shadow">
-      {{ props.text }}
-    </UButton>
+    <NuxtLink :to="{ path: '/', hash: props.hash }">
+      <UButton
+        class="wedding-300 absolute top-[calc(83%)] left-1/2 -translate-x-1/2 text-slate-200 p-5 font-alegreya font-bold text-xl wedding-box-shadow">
+        {{ props.text }}
+      </UButton>
+    </NuxtLink>
   </div>
 </template>
 
@@ -13,5 +15,9 @@ const props = defineProps({
     type: String,
     default: '',
   },
+  hash: {
+    type: String,
+    default: '',
+  }
 });
 </script>
