44a3557be54652735b4ceb2929d9c430ebcd46cb
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-03-07 20:20:04 +0100

    revert: rezygnacja z dodawania zdjęć przez stronę, strona niech będzie tylko informacyjną, nie chcemy psuć roboty fotografowi :P

--- components/Main.vue
+++ components/Main.vue
@@ -1,11 +1,8 @@
 <template>
-    <WeddingDay v-if="weddingDay" @image-loaded="onImageLoaded" :is-loading="isLoading" />
-    <Hero v-else @image-loaded="onImageLoaded" :is-loading="isLoading" />
+    <Hero @image-loaded="onImageLoaded" :is-loading="isLoading" />
 </template>
 
 <script setup lang="ts">
-const { countDown } = useCountDown();
-const weddingDay = ref(false);
 const emit = defineEmits(['image-loaded']);
 const onImageLoaded = () => {
     emit('image-loaded');
@@ -13,11 +10,5 @@ const onImageLoaded = () => {
 
 const props = defineProps({
     isLoading: Boolean,
-});
-
-onMounted(() => {
-    const today = new Date().setHours(0, 0, 0, 0);
-    const weddingDate = countDown.setHours(0, 0, 0, 0);
-    weddingDay.value = today >= weddingDate;
 });
 </script>
