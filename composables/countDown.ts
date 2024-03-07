3e71088df71592926d62d2f3065e323dc269706b
Rafał Wawiórka <rafal.wawiorka@contelizer.pl>
2024-03-07 20:19:22 +0100

    fix: ustawiono poprawną datę ślubu

--- composables/countDown.ts
+++ composables/countDown.ts
@@ -1,5 +1,5 @@
 export const useCountDown = () => {
-  const countDown = new Date(2024, 8, 14);
+  const countDown = new Date(2024, 8, 14, 15, 0, 0, 0);
   return {
     'countDown': countDown,
   }
