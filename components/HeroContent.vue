4a9e4687277bd15790d8038f1992cbd889ca632a
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-03-25 19:38:19 +0100

    chore: removed slogan

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="w-full h-full absolute top-0">
-    <blockquote
-        class="hero-slogan text-slate-200 text-center absolute left-1/2 -translate-x-1/2 -translate-y-1/2 font-cloud w-[calc(100%-30px)] text-xl top-[calc(10%)]">
-      <sub class="relative">
-        <FormatQuote class="absolute right-full bottom-0"/>
-      </sub>
-      {{ heroData.slogan }}
-      <sup class="relative">
-        <FormatQuote class="absolute left-full top-0 rotate-180"/>
-      </sup>
-    </blockquote>
-
     <h1 class="hero-title font-bold absolute top-[calc(40%)] left-1/2 -translate-x-1/2 -translate-y-1/2 font-cloud primary-color wedding-text-shadow text-center text-6xl">
       {{ heroData.text }}
     </h1>
@@ -21,13 +10,11 @@
 </template>
 
 <script setup lang="ts">
-import FormatQuote from '~icons/material-symbols/format-quote';
 const {countDown} = useCountDown();
 import {ref} from 'vue';
 
 const heroData = ref({
-  text: "Paulina i Rafał",
-  slogan: "Miłość sama w sobie jest nie do pojęcia, ale dzięki miłości możemy pojąć wszystko.",
+  text: "Paulina & Rafał",
   date: countDown.toLocaleDateString('pl-PL', {
     year: 'numeric',
     month: 'numeric',
