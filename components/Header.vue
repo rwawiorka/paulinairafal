e7e3176afe173eae6f98781bb951927e492e33b1
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-03-25 19:37:18 +0100

    feat: added header, menu

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="absolute w-full z-10 mt-4 pb-4 border-b border-opacity-20 border-wedding-50-50">
+    <div class="container m-auto flex justify-between items-center font-cloud">
+      <h3 class="title inline font text-2xl"><a href="/">Paulina &amp; Rafał</a></h3>
+      <div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="icon icon-bars"></i></div>
+      <Menu/>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
---
+++ components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="main-menu visible-on-click flex justify-center items-center gap-4 text-lg font-alegreya" id="main-menu">
+    <li class="main-menu-item">
+      <a href="/">Strona główna</a>
+    </li>
+    <li class="main-menu-item">
+      <a href="#maps">Lokalizacja</a>
+    </li>
+    <li class="main-menu-item">
+      <a href="#rvps">Potwierdzenie przybycia</a>
+    </li>
+    <li class="main-menu-item">
+      <a href="#gallery">Galeria</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
