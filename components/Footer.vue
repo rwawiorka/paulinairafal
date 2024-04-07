52383905f2b997f193e3bd34ac933128a70a4e37
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 23:22:19 +0200

    adjustments to menu mobile

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,36 @@
 <template>
-  <footer class=" w-full z-10 pt-6 pb-6 border-t border-opacity-20 border-wedding-50-50 bg-wedding-600" id="main">
-    <div class="container m-auto flex justify-between items-center font-cloud">
+  <footer class=" w-full z-10 p-6 pt-6 pb-6 border-t border-opacity-20 border-wedding-50-50 bg-wedding-400" id="main">
+    <div class="container m-auto flex items-center font-cloud" :class="{'justify-center': isMobile, 'justify-between': !isMobile}">
       <h3 class="title inline font text-2xl"><a href="/">Paulina &amp; Rafał</a></h3>
-      <div class="menu-nav-icon" data-nav-menu="#main-menu"></div>
-      <Menu/>
+      <Menu v-if="!isMobile"/>
     </div>
   </footer>
 </template>
 
 <script lang="ts" setup>
+
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const isMobile = ref(false);
+
+const updateIsMobile = () => {
+  if (typeof window !== 'undefined') {
+    isMobile.value = window.matchMedia('(max-width: 767px)').matches;
+  }
+};
+
+onMounted(() => {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('resize', updateIsMobile);
+    updateIsMobile();
+  }
+});
+
+onUnmounted(() => {
+  if (typeof window !== 'undefined') {
+    window.removeEventListener('resize', updateIsMobile);
+  }
+});
 
 </script>
 
--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,37 @@
 <template>
-  <header class="absolute w-full z-10 pt-6 pb-6 border-b border-opacity-20 border-wedding-50-50" id="main">
+  <header class="absolute w-full z-10 p-6 pt-6 pb-6 border-b border-opacity-20 border-wedding-50-50" id="main">
     <div class="container m-auto flex justify-between items-center font-cloud">
       <h3 class="title inline font text-2xl"><a href="/">Paulina &amp; Rafał</a></h3>
-      <div class="menu-nav-icon" data-nav-menu="#main-menu"></div>
-      <Menu/>
+      <MenuMobile v-if="isMobile"  />
+      <Menu v-else />
+      
     </div>
   </header>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue';
 
+const isMobile = ref(false);
+
+const updateIsMobile = () => {
+  if (typeof window !== 'undefined') {
+    isMobile.value = window.matchMedia('(max-width: 767px)').matches;
+  }
+};
+
+onMounted(() => {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('resize', updateIsMobile);
+    updateIsMobile(); // Call this function to set the initial value of isMobile
+  }
+});
+
+onUnmounted(() => {
+  if (typeof window !== 'undefined') {
+    window.removeEventListener('resize', updateIsMobile);
+  }
+});
 </script>
 
 <style></style>
