3feee78b9908fbd03b7a0fe9ffd9d965919fdafe
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 20:11:02 +0200

    adjusted hero content

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full h-full absolute top-0">
-    <h1 class="hero-title font-bold absolute top-[calc(40%)] left-1/2 -translate-x-1/2 -translate-y-1/2 font-cloud primary-color wedding-text-shadow text-center text-6xl">
+    <h1 class="hero-title absolute top-[calc(40%)] left-1/2 -translate-x-1/2 -translate-y-1/2 font-cloud primary-color wedding-text-shadow text-center text-8xl">
       {{ heroData.text }}
     </h1>
-    <h2 class="hero-date text-slate-100 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 font-alegreya text-4xl top-[calc(55%)]">
+    <h2 class="hero-date text-slate-100 absolute left-1/2 -translate-x-1/2 -translate-y-1/2 font-cloud text-6xl top-[calc(55%)]">
       {{ heroData.date }}
     </h2>
   </div>
