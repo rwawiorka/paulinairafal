6003908d8f788ac5f6269fd3a44982a5392ff6d9
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 20:11:23 +0200

    added footer

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class=" w-full z-10 pt-6 pb-6 border-t border-opacity-20 border-wedding-50-50 bg-wedding-600" id="main">
+    <div class="container m-auto flex justify-between items-center font-cloud">
+      <h3 class="title inline font text-2xl"><a href="/">Paulina &amp; Rafał</a></h3>
+      <div class="menu-nav-icon" data-nav-menu="#main-menu"></div>
+      <Menu/>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
