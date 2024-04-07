58fe69ab2628fd16975ca4a0cb835fde0be53634
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 20:10:37 +0200

    fix: adjusted header

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="absolute w-full z-10 mt-4 pb-4 border-b border-opacity-20 border-wedding-50-50">
+  <header class="absolute w-full z-10 pt-6 pb-6 border-b border-opacity-20 border-wedding-50-50" id="main">
     <div class="container m-auto flex justify-between items-center font-cloud">
       <h3 class="title inline font text-2xl"><a href="/">Paulina &amp; Rafał</a></h3>
-      <div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="icon icon-bars"></i></div>
+      <div class="menu-nav-icon" data-nav-menu="#main-menu"></div>
       <Menu/>
     </div>
   </header>
