9ae2f51b9f91f96cb51d55b762389e3fd8ae2951
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 20:11:57 +0200

    added timeline

---
+++ components/Timeline.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="timeline" class="p-16" :style="{ backgroundImage: `url(${imgSrc})` }">
+    <div
+      class="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+      <TimelineItem title="Ślub" time="15:30" location="Parafia Rzymskokatolicka p.w. Podwyższenia Krzyża Świętego w Bargłowie Kościelnym" icon="icon-ciurclke" />
+      <TimelineItem title="Wesele" location="Sala Bankietowa Uroczysko Nowiny Bargłowskie 3" icon="icon-wine-glass" reverse="true" />
+      <TimelineItem title="Poprawiny" location="Sala Bankietowa Uroczysko Nowiny Bargłowskie 3" icon="icon-wine-glass" isSvg="true" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const imgSrc = '/timeline-bg.jpg';
+</script>
+
+<style></style>
---
+++ components/TimelineItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active gap-5"
+    :class="{ 'flex': reverse, 'justify-end': reverse }">
+    <!-- Icon -->
+    <div
+      class="flex items-center justify-center w-5 h-5 rounded-full border border-wedding-400 bg-white text-slate-500 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+    </div>
+    <!-- Card -->
+    <div class="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] p-4">
+      <svg v-if="isSvg" xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="#fd6b9a" class="bi bi-signpost-split"
+        viewBox="0 0 16 16">
+        <path
+          d="M7 7V1.414a1 1 0 0 1 2 0V2h5a1 1 0 0 1 .8.4l.975 1.3a.5.5 0 0 1 0 .6L14.8 5.6a1 1 0 0 1-.8.4H9v10H7v-5H2a1 1 0 0 1-.8-.4L.225 9.3a.5.5 0 0 1 0-.6L1.2 7.4A1 1 0 0 1 2 7h5zm1 3V8H2l-.75 1L2 10h6zm0-5h6l.75-1L14 3H8v2z">
+        </path>
+      </svg>
+      <i v-else class="icon-timeline" :class="[icon, { 'flex': reverse, 'justify-end': reverse }]"></i>
+      <div class="font-bold text-slate-900 text-2xl" :class="{ 'flex': reverse, 'justify-end': reverse }">{{ title }}
+      </div>
+      <time v-if="time" class="font-caveat font-medium text-slate-900 text-2xl"
+        :class="{ 'flex': reverse, 'justify-end': reverse }">{{ time }}</time>
+      <div v-if="location" class="text-slate-500 text-xl" :class="{ 'flex': reverse, 'justify-end': reverse }">{{
+      location }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  time: {
+    type: String,
+    default: '',
+  },
+  location: {
+    type: String,
+    default: '',
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+  reverse: {
+    type: Boolean,
+    default: false
+  },
+  isSvg: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<style></style>
