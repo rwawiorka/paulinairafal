c661d83acc52009b009a733ab5c780e1fee54c2d
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2024-04-07 20:11:50 +0200

    added service item

---
+++ components/ServiceItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3 class="text-3xl mb-8">{{ name }}</h3>
+    <div class="flex justify-center items-center gap-4">
+      <a v-if="fb" :href="fb" target="_blank" rel="noopener noreferrer">
+        <i class="icon-timeline icon-facebook" />
+      </a>
+      <a v-if="ig" :href="ig" target="_blank" rel="noopener noreferrer">
+        <i class="icon-timeline icon-instagram" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  icon: {
+    type: Object,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  fb: {
+    type: String,
+    default: '',
+  },
+  ig: {
+    type: String,
+    default: '',
+  },
+})
+
+</script>
+
+<style></style>
